ee13f5555495e75311b7240512ba1bdfa500e6a2
Razgriz <gabriel_henriq.1@hotmail.com>
2023-04-04 15:19:17 -0300

    Login e Cadsatro

---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,73 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade Manipular Produtos API ServRest', () => {
+    let token
+    before(() => {
+        cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+    })
+
+
+    it('Listar Produtos', () => {
+        cy.request({
+            method:'GET',
+            url:'http://localhost:3000/produtos',
+            
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body).to.have.property('produtos')
+            expect(response.duration).to.be.lessThan(50)
+        })
+        
+    
+    })
+
+    it('Cadastrar Produto', () => {
+        let produto=`Livro ${Math.floor(Math.random() * 100000)}`
+        cy.request({
+            method:'POST',
+            url:'http://localhost:3000/produtos',
+            body: {
+                    "nome": produto,
+                    "preco": 50,
+                    "descricao": "Livro",
+                    "quantidade": 5000
+                },
+                headers: {authorization:token}
+            
+        }).then((response) => {
+            expect(response.status).to.equal(201)
+            expect(response.body.message).to.have.equal('Cadastro realizado com sucesso')
+            expect(response.body).to.have.property('_id')
+            expect(response.duration).to.be.lessThan(50)
+        })
+
+
+    })
+
+
+
+
+    it('Excluir Produto', () => {
+        cy.request({
+            method:'GET',
+            url:'http://localhost:3000/produtos',
+            
+        }).then(response => {
+            let id = response.body.produtos[0]._id
+            cy.request({
+            method:'DELETE',
+            url:`http://localhost:3000/produtos/${id}`,
+            headers: {authorization:token},
+
+    
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.duration).to.be.lessThan(50)
+            expect(response.body.message).to.equal('Registro excluído com sucesso')
+        })
+    
+        })
+
+    })
+
+})
